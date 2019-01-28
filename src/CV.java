1b2c8ca55ece1b20c82f7a5365f4b150f473b947
Ouellette Maxime <maximeouellette00@gmail.com>
2019-01-28 13:40:09 -0500

    Creation de la classe CV , ajout des constructeurs

--- src/CV.java
+++ src/CV.java
@@ -1,4 +1,26 @@
 
 public class CV {
-
+	
+	
+	public String nom;
+	public String prenom;
+	public String formation;
+	public int experiences;
+	public String[] competences;
+	public String attentes;
+	
+	// Constructeur CV base
+	public CV(){
+		
+	}
+	
+	public CV( String pNom, String pPrenom, String pFormation, int pExperiences, String[] pCompetences, String pAttentes){
+		
+		nom = pNom;
+		prenom = pPrenom;
+		formation = pFormation;
+		experiences = pExperiences;
+		competences = pCompetences;
+		attentes = pAttentes;
+	}
 }
